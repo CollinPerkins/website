75a317a423e66af847d0fef76d6fa2de9fb996df
Collin Perkins <collinperkinsweb@gmail.com>
2016-06-25 23:48:05 -0500

    finished Quote Machine

--- random-quote-machine/main.js
+++ random-quote-machine/main.js
@@ -1,8 +1,27 @@
+window.twttr = (function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0],
+  t = window.twttr || {};
+if (d.getElementById(id)) return t;
+js = d.createElement(s);
+js.id = id;
+js.src = "https://platform.twitter.com/widgets.js";
+fjs.parentNode.insertBefore(js, fjs);
+
+t._e = [];
+t.ready = function(f) {
+  t._e.push(f);
+};
+
+return t;
+}(document, "script", "twitter-wjs"));
+
 $(document).ready(function(){
   setTimeout(function () {
     ajaxCall();
   }, 1000);
   $('#newQuote').click(function() {
+    $( "#tweet" ).remove();
+    $( "#quoteArea" ).append( "<div id='tweet'></div>" );
     ajaxCall();
   });
 });
@@ -18,7 +37,15 @@ function ajaxCall() {
       quote = data.quoteText;
       author = data.quoteAuthor;
       $('#quote').html(quote);
-      $('#author').html(author);
+      $('#author').html('-' + author + '-');
+      twttr.widgets.createShareButton(
+      'https://dev.twitter.com/',
+      document.getElementById('tweet'),
+      {
+        text: quote + '\n' + '-' + author + '-' + '\n'
+      }
+    );
     }
   });
+
 }
